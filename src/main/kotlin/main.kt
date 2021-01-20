a588516285901642f6aa2135158ddae96c5d5f14
slugskickass <a.cadby@sheffield.ac.uk>
2021-01-20 10:45:58 +0000

    Added a loop for updating the stock price and started to build the state dependecies

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,43 +1,37 @@
 import androidx.compose.desktop.Window
-import androidx.compose.runtime.*
-import androidx.compose.ui.graphics.ImageBitmap
-import androidx.compose.ui.graphics.asImageBitmap
-import org.apache.commons.csv.CSVFormat
-
-import org.apache.commons.csv.CSVParser
-import org.jetbrains.skija.Image
-import pl.zankowski.iextrading4j.client.IEXCloudTokenBuilder
-import pl.zankowski.iextrading4j.client.IEXTradingApiVersion
-import pl.zankowski.iextrading4j.client.IEXTradingClient
-import pl.zankowski.iextrading4j.client.rest.request.stocks.QuoteRequestBuilder
-import java.nio.file.Files
-import java.nio.file.Paths
-import pl.zankowski.iextrading4j.client.rest.request.stocks.LogoRequestBuilder
-
-import pl.zankowski.iextrading4j.api.stocks.Logo
-import pl.zankowski.iextrading4j.client.IEXCloudClient
-import java.io.File
-import java.nio.file.StandardCopyOption
-
-import java.io.InputStream
-import java.net.URL
-
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.ScrollableColumn
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import pl.zankowski.iextrading4j.api.refdata.v1.Currency
-import pl.zankowski.iextrading4j.api.stocks.v1.PriceTarget
-import pl.zankowski.iextrading4j.client.rest.request.stocks.PriceRequestBuilder
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVParser
+import org.jetbrains.skija.Image
+import pl.zankowski.iextrading4j.api.stocks.Logo
+import pl.zankowski.iextrading4j.client.IEXCloudClient
+import pl.zankowski.iextrading4j.client.IEXCloudTokenBuilder
+import pl.zankowski.iextrading4j.client.IEXTradingApiVersion
+import pl.zankowski.iextrading4j.client.IEXTradingClient
+import pl.zankowski.iextrading4j.client.rest.request.stocks.LogoRequestBuilder
+import pl.zankowski.iextrading4j.client.rest.request.stocks.QuoteRequestBuilder
+import java.io.File
+import java.io.InputStream
 import java.math.BigDecimal
 import java.math.RoundingMode
+import java.net.URL
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.nio.file.StandardCopyOption
+import kotlin.concurrent.fixedRateTimer
 
 
 val basefilename = "/Users/Ashley/IdeaProjects/IEXCloudVisual/src/resources/\n"
@@ -45,20 +39,22 @@ val basefilename = "/Users/Ashley/IdeaProjects/IEXCloudVisual/src/resources/\n"
 
 fun main() = Window() {
     val cloudClient = IEXTradingClient.create(
-        IEXTradingApiVersion.IEX_CLOUD_STABLE,
-        IEXCloudTokenBuilder().withPublishableToken(token).build()
+        IEXTradingApiVersion.IEX_CLOUD_STABLE_SANDBOX,
+        IEXCloudTokenBuilder().withPublishableToken(tokens).build()
     )
     val csvData = "src/resources/stocks.csv"
+
+  //  var out = remember { mutableStateOf(initialstocks(loadcsv(csvData), cloudClient)) }
+
     var out = initialstocks(loadcsv(csvData), cloudClient)
-
-
+    playtimer(out, cloudClient)
 
 
     Scaffold(
         topBar = { TopAppBar(title = {Text("The total value of the Portfolio is ${gettotal(out).setScale(2, RoundingMode.HALF_DOWN).toString()}")},backgroundColor = Color.LightGray)  },
         drawerContent = { Text(text = "drawerContent") },
         bodyContent = { buildstocks(out, cloudClient) },
-        bottomBar = { BottomAppBar(backgroundColor = Color.DarkGray) { Text("BottomAppBar") } }
+        bottomBar = { BottomAppBar(backgroundColor = Color.DarkGray) { displayButtons(out, cloudClient) }} //Button(modifier = Modifier.align(Alignment.CenterVertically), onClick = {println("test")}){Text("Hello")}
     )
 
     }
@@ -115,6 +111,20 @@ fun initialstocks(stocks: MutableList<Stock>, cloudClient: IEXCloudClient): Muta
     return stocks
 }
 
+fun playtimer(stocks: MutableList<Stock>, cloudClient: IEXCloudClient) {
+    val fixedRateTimer = fixedRateTimer(name = "my timer", initialDelay = 10000, period = 30000){
+        updateprice(stocks, cloudClient)
+    }
+}
+
+fun updateprice(stocks: MutableList<Stock>, cloudClient: IEXCloudClient) {
+    stocks.map {it.current_val = getcurrentvalue(it.name, cloudClient) }
+    stocks.map {it.current_holding = it.current_val * it.number_held.toBigDecimal()}
+    print(stocks.component1().name)
+    print(" ")
+    println(stocks.component1().current_val)
+}
+
 fun getcurrentvalue(name: String, cloudClient: IEXCloudClient): BigDecimal {
     val quote = cloudClient.executeRequest(QuoteRequestBuilder().withSymbol(name).build())
     //val quote = cloudClient.executeRequest(PriceRequestBuilder().withSymbol(name).build())
@@ -128,6 +138,10 @@ fun getcurrentvalue(name: String, cloudClient: IEXCloudClient): BigDecimal {
 fun gettotal(stocks: MutableList<Stock>): BigDecimal {
     val total_value = stocks.sumOf { it.current_holding }
 return total_value
+}
+
+fun printoutstocks(stocks: MutableList<Stock>){
+    stocks.forEach { println("Name: ${it.name} current value is ${it.current_val}") }
 }
 
 @Composable fun buildstocks(out: MutableList<Stock>, cloudClient: IEXCloudClient){
@@ -183,5 +197,31 @@ return total_value
             }
         }
     }
+}
+
+@Composable fun displayButtons(stocks: MutableList<Stock>, cloudClient: IEXCloudClient){
+    Row(){
+        updatebutton(stocks, cloudClient)
+        loadbutton()
+        savebutton()
+        testbutton(stocks)
+    }
+}
+
+@Composable fun updatebutton(stocks: MutableList<Stock>, cloudClient: IEXCloudClient){
+    Button( onClick = {updateprice(stocks, cloudClient)
+        println("test")     }){Text("Update")}
+}
+
+@Composable fun loadbutton(){
+    Button( onClick = {println("test")}){Text("Load")}
+}
+
+@Composable fun savebutton(){
+    Button( onClick = {println("test")}){Text("Save")}
+}
+
+@Composable fun testbutton(stocks: MutableList<Stock>){
+    Button( onClick = {printoutstocks(stocks)}){Text("Test")}
 }
 
